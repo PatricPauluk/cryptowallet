29244de177d6fca865fdff5b2b6e8d710dd5ef74
Patric Pauluk <patric.pauluk@outlook.com>
2024-10-17 11:17:21 -0300

    my first custom helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Meu primeiro helper, modifica o método Date.today para ser exibido no formato brasileiro
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%y")
+  end
 end
