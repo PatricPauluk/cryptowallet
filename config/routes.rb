7938de1b52c5fafdc77e5121fd66c57b51cbad6e
Patric Pauluk <patric.pauluk@outlook.com>
2024-10-17 15:16:17 -0300

    learning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+
+  get '/inicio', to: 'welcome#index' # Acessa o welcome/index também em => http://localhost:3000/inicio
+
   resources :coins
+  # Rota raiz criada para redirecionar inicialmente para welcome/index
+  root to: 'welcome#index' # => get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
