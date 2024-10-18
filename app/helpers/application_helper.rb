69a747c9b58c8d718b84e6698ce2e340def6152b
Patric Pauluk <patric.pauluk@outlook.com>
2024-10-18 20:17:05 -0300

    translate rails enviroment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,15 @@ module ApplicationHelper
   def data_br(data_us)
     data_us.strftime("%d/%m/%y")
   end
+
+  # Traduz o ambiente de desenvolviento atual de inglês para português brasileiro
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Testes"
+    end
+  end
 end
