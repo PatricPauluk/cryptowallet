6a410165f99fee7f4e9c35f07641a852e4912e5b
Patric Pauluk <patric.pauluk@outlook.com>
2024-10-31 21:35:25 -0300

    associate coin to mining type

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,2 +1,12 @@
 class Coin < ApplicationRecord
+  # Especifica que o model Coin vai se comunicar com o model MiningType (necessário quando tem o ID de outra tabela (FK))
+  belongs_to :mining_type
+  # |-------------------------------------------------------------------------------------------------------------------|
+  # | Comandos no terminal (rails c):                                                                                   |
+  # | c = Coin.first                                                                                                    |
+  # | m = MiningType.first                                                                                              |
+  # | c.mining_type = m                                                                                                 |
+  # |                                                                                                                   |
+  # | O Rails automaticamente detecta que você esta passando o ID do MiningType selecionado, mesmo sem especificar o ID |
+  # |-------------------------------------------------------------------------------------------------------------------|
 end
