137d15375ce9a82d0bf9195000a92cdcf4d8be96
Patric Pauluk <patric.pauluk@outlook.com>
2024-10-31 22:59:24 -0300

    coin belongs to mining_type, and mining_type has many coins :)

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,12 +1,17 @@
 class Coin < ApplicationRecord
   # Especifica que o model Coin vai se comunicar com o model MiningType (necessário quando tem o ID de outra tabela (FK))
-  belongs_to :mining_type
+  belongs_to :mining_type # belongs_to: Uma coin pode pertencer a apenas um mining_type
+end
+
   # |-------------------------------------------------------------------------------------------------------------------|
   # | Comandos no terminal (rails c):                                                                                   |
   # | c = Coin.first                                                                                                    |
   # | m = MiningType.first                                                                                              |
   # | c.mining_type = m                                                                                                 |
   # |                                                                                                                   |
-  # | O Rails automaticamente detecta que você esta passando o ID do MiningType selecionado, mesmo sem especificar o ID |
+  # | O Rails automaticamente detecta que você esta passando o ID do MiningType selecionado, mesmo sem especificar o    |
+  # | ID. Após gravar no banco com c.save, eu consigo pesquisar quantas coins estão associadas a um mining type com o   |
+  # | comando: m.coins                                                                                                  |
+  # | Caso já tenha uma moeda selecionada em C, você pode "jogar" ela no MiningType para também inserir o id com o      |
+  # | comando: m.coins << c                                                                                             |
   # |-------------------------------------------------------------------------------------------------------------------|
-end
--- app/models/mining_type.rb
+++ app/models/mining_type.rb
@@ -1,2 +1,3 @@
 class MiningType < ApplicationRecord
+  has_many :coins # has_many: Um mining_type pode pertencer a várias coins
 end
