210052447afbec88e60631aac926fdabe679f39a
Patric Pauluk <patric.pauluk@outlook.com>
2024-12-06 20:42:13 -0300

    using the app with 2 languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  # Todas as requisições passam por aqui
+
+  before_action :set_locale
+
+  def set_locale
+      # Se existir um parâmetro chamado locale...
+      if params[:locale]
+        cookies[:locale] = params[:locale]
+      end
+
+      # Se existir um cookie chamado locale...
+      if cookies[:locale]
+        if I18n.locale != cookies[:locale]
+          I18n.locale = cookies[:locale]
+        end
+      end
+  end
 end
