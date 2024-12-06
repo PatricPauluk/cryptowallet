0c580536927da09bbf7396298f282ae5eabd4bb8
Patric Pauluk <patric.pauluk@outlook.com>
2024-12-06 20:09:50 -0300

    cookies and sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @nome = "Patric"
-
+    cookies[:curso] = "Curso de Ruby on Rails [Cookie]" # Criação de um cookie
+    session[:curso] = "Curso de Ruby on Rails [Session]" # Criação de um cookie
     # Recebe um parâmetro e exibe na view index
     @parametro = params[:parametro] # Exemplo: ?parametro=Olá mundo!
   end
